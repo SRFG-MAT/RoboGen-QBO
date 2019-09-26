5dc939158aec28551af2db2f5f514422968af361
root <mathias.schmoigl@salzburgresearch.at>
2019-09-26 12:55:03 +0200

    changed debug level to 3 in alexa sample app call

--- Alexa/startsample.sh
+++ Alexa/startsample.sh
@@ -1,3 +1,3 @@
   cd "/home/pi/build/SampleApp/src"
 
-  ./SampleApp "/home/pi/build/Integration/AlexaClientSDKConfig.json" "/home/pi/third-party/alexa-rpi/models" DEBUG9
+  ./SampleApp "/home/pi/build/Integration/AlexaClientSDKConfig.json" "/home/pi/third-party/alexa-rpi/models" DEBUG3
