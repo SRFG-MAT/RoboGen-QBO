47149d66f3af9624561d8e083efb926706dce7e1
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2020-01-20 15:34:34 +0100

    minor refactoring preparations for decision tree implementations

---
+++ Python_Projects/DecisionTrees/Processing_Audio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import speech_recognition as sr
+
+#---------------------------------------------------------------------------------------------
+# getAudioToText - (use Google lib to get microphone input as text)
+#---------------------------------------------------------------------------------------------
+def getAudioToText():
+    recognizer = sr.Recognizer()
+
+    while True:
+        
+        with sr.Microphone() as source:          
+            
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+
+        try:
+            return recognizer.recognize_google(audio, language="de-AT")
+        
+        except sr.UnknownValueError:
+            print("------------------------------------------------------")
+            print("Die Sprachaufzeichnung konnte dich leider nicht verstehen")
+            print("------------------------------------------------------")
+        except sr.RequestError as e:
+            print("------------------------------------------------------")
+            print("Konnte kein Ergebnis von Google erhalten; {0}".format(e))
+            print("------------------------------------------------------")
