e6575a8b6dd99c596d20390cf29f675b0b93d261
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2020-06-09 12:22:47 +0200

    bluetooth connection my now be ended and recreated if connection button pressed again. Shutdown will not stop program anymore

--- Python_Projects/BlueToothAdapter/BlueToothAdapter.py
+++ Python_Projects/BlueToothAdapter/BlueToothAdapter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 import os, sys, time
-time.sleep(25) # wait for ubuntu bluetoothservice to be init
+time.sleep(25) # wait for ubuntu bluetoothservice to be init (TODO: maybe change this to wait until service is ready)
 
 import bluetooth
 import subprocess
@@ -9,7 +9,21 @@ import subprocess
 sys.path.append('/opt/QBO/RoboGen-QBO/Python_Projects/MyQBOSettings')
 import SettingsReader
 
+# -------------------------------------------------------------------------------------------
+# defining criteria for listening socket
+# -------------------------------------------------------------------------------------------
+BUFF_SIZE = 4096 # 4 KiB
+port = 1
 
+# -------------------------------------------------------------------------------------------
+# helper function: create the socket and bind to it
+# -------------------------------------------------------------------------------------------
+def createSocketAndBind():
+    socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    socket.bind(("",port))
+    socket.listen(1)
+    print("Socket created succesfully. Port is %i " % port)
+    return socket
 
 # -------------------------------------------------------------------------------------------
 # helper function: wait for accepted socket
@@ -20,55 +34,40 @@ def waitForConnection(server_socket):
     print("Accepted connection from ", address)
     return client_socket,address
 
-
-# -------------------------------------------------------------------------------------------
-# defining criteria for listening socket
-# -------------------------------------------------------------------------------------------
-server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-port = 1
-server_socket.bind(("",port))
-server_socket.listen(1)
-print("Socket created succesfully. Port is %i " % port)
-
-# wait for connection
-client_socket,address = waitForConnection(server_socket)
-BUFF_SIZE = 4096 # 4 KiB
-
-
 # -------------------------------------------------------------------------------------------
 # listen in endless loop
 # -------------------------------------------------------------------------------------------
 while True:
-    
-    try:
-        data = None  
-        req = client_socket.recv(BUFF_SIZE)
-                
-        # check for length
-        if len(req) == 0: break
-        elif len(req) == 1 and ord(req[0]) == 113: # 113 == 'q'
-            print ("Shutdown Android Communication")
-            sys.exit()
-                
-        # check if send response             
-        #if req in ('temp', '*temp'):
-        #   data = str(random.random())+'!'            
-        #   client_socket.send(data)
-                    
-        # write json               
-        SettingsReader.writeJsonFile(req)
-        client_socket.send("Daten erfolgreich erhalten!")
+    server_socket = createSocketAndBind() 
+    client_socket,address = waitForConnection(server_socket)   
 
-    except IOError: pass
+    while True:
     
-    except KeyboardInterrupt:         
-        client_socket.close()
-        client_socket.close()
-        print "user forced close: cleanup all done"
-        break
+        try:
+            data = None  
+            req = client_socket.recv(BUFF_SIZE)
+                
+            # check for length
+            if len(req) == 0: break
+            elif len(req) == 1 and ord(req[0]) == 113: # 113 == 'q'
+                print ("Shutdown Android Communication")
+                server_socket.close()
+                break        
+                    
+            # write json               
+            SettingsReader.writeJsonFile(req)
+            client_socket.send("Daten erfolgreich erhalten!")
+
+        except IOError: pass
     
-    except bluetooth.btcommon.BluetoothError as btError:      
-        print('Lost accepted connection...')
-        client_socket,address = waitForConnection(server_socket)
-        continue
+        except KeyboardInterrupt:         
+            client_socket.close()
+            client_socket.close()
+            print "user forced close: cleanup all done"
+            break
+    
+        except bluetooth.btcommon.BluetoothError as btError:      
+            print('Lost accepted connection...')
+            client_socket,address = waitForConnection(server_socket)
+            continue
  
