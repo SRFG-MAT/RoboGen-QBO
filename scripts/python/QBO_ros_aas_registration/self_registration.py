5aeadd04a840e7248a1997ed314129261548d808
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2021-08-06 09:04:30 +0200

    added folder and first files for AAS self description

---
+++ scripts/python/QBO_ros_aas_registration/self_registration.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf-8
+import requests
+import json
+
+
+#---------------------------------------------------------------------------------------------
+# loadAASType
+#---------------------------------------------------------------------------------------------
+def loadAASType():
+
+    with open('/opt/QBO/catkin_ws/src/RoboGen-QBO/scripts/python/QBO_ros_aas_registration/QBO_type.json', 'r') as dt_file:
+        dt_data = json.load(dt_file) 
+        # TODO
+    
+    return dt_data
+
+#---------------------------------------------------------------------------------------------
+# loadAASInstance
+#---------------------------------------------------------------------------------------------
+def loadAASInstance():
+    
+    with open('/opt/QBO/catkin_ws/src/RoboGen-QBO/scripts/python/QBO_ros_aas_registration/QBO_instance.json', 'r') as dt_file:
+        dt_data = json.load(dt_file) 
+        # TODO
+        
+    return dt_data
+
+# -------------------------------------------------------------------------------------------
+# uploadAAS (upload full settings with all entries)
+# -------------------------------------------------------------------------------------------
+def uploadAAS(aas):
+    
+    try:     
+        #r = requests.post('https://power2dm.salzburgresearch.at/robogen/DataBase/UploadJSON_MySettings', timeout=5, verify=False, json=settings)
+        r = requests.put(settings_endpoint, timeout=5, verify=False, json=aas)
+        headers = {'Content-type': 'application/json'}      
+            
+        if r.ok:
+            print("Description uploaded succesfullly")
+        else:
+            print("--------------------------")
+            print("Error in server response: " + str(r.status_code))
+            print("--------------------------")
+                        
+    except requests.exceptions.RequestException as e:
+        print e
+        
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+# main function
+#------------------------------------------------------------------------------------------------------------------------------------------------------------	 
+if __name__ == '__main__':
+    
+    aas_type = loadAASType()
+    aas_instance = loadAASInstance()
+    uploadAAS(aas_type)
+    uploadAAS(aas_instance)
+    
+    
+    
+    
