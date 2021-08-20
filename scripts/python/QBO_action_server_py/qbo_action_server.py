05bd50ea650b9eb65983d81da954218e7cfb6dc6
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2021-08-20 13:47:54 +0200

    added GetData and SetData Actions to QBO Action Server

--- scripts/python/QBO_action_server_py/qbo_action_server.py
+++ scripts/python/QBO_action_server_py/qbo_action_server.py
@@ -5,44 +5,51 @@ import actionlib
 import robogenqbo.msg
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
+# helper function: fibonacci_example
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+def fibonacci_example(self):
+    
+    r = rospy.Rate(1)
+    success = True
+    
+    for i in range(1, goal.order):
+        # check that preempt has not been requested by the client
+        if self._as.is_preempt_requested():
+            rospy.loginfo('%s: Preempted' % self._action_name)
+            self._as.set_preempted()
+            success = False
+            break
+            
+        self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])           
+        self._as.publish_feedback(self._feedback) # publish the feedback 
+        r.sleep() # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+    
+    return success
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
 # VoiceOutput
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 class VoiceOutput(object):
-    # create messages that are used to publish feedback/result
-    _feedback = robogenqbo.msg.VoiceOutputFeedback()
-    _result = robogenqbo.msg.VoiceOutputResult()
+    
+    _feedback = robogenqbo.msg.VoiceOutputFeedback() # create feedback message
+    _result = robogenqbo.msg.VoiceOutputResult() # create result message
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.VoiceOutput, execute_cb=self.execute_cb, auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.VoiceOutputAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
       
     def execute_cb(self, goal):
-        # helper variables
-        r = rospy.Rate(1)
-        success = True
         
         # append the seeds for the fibonacci sequence
         self._feedback.sequence = []
         self._feedback.sequence.append(0)
         self._feedback.sequence.append(1)
         
-        # publish info to the console for the user
         rospy.loginfo('%s: Executing, creating VoiceOutput sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
         
         # start executing the action
-        for i in range(1, goal.order):
-            # check that preempt has not been requested by the client
-            if self._as.is_preempt_requested():
-                rospy.loginfo('%s: Preempted' % self._action_name)
-                self._as.set_preempted()
-                success = False
-                break
-            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-            # publish the feedback
-            self._as.publish_feedback(self._feedback)
-            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-            r.sleep()
+        success = fibonacci_example(self, success)
           
         if success:
             self._result.sequence = self._feedback.sequence
@@ -53,41 +60,86 @@ class VoiceOutput(object):
 # WaitForUserInput
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 class WaitForUserInput(object):
-    # create messages that are used to publish feedback/result
-    _feedback = robogenqbo.msg.WaitForUserInputFeedback()
-    _result = robogenqbo.msg.WaitForUserInputResult()
+    
+    _feedback = robogenqbo.msg.WaitForUserInputFeedback() #create feedback message
+    _result = robogenqbo.msg.WaitForUserInputResult() #create result message
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.WaitForUserInput, execute_cb=self.execute_cb, auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.WaitForUserInputAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
       
     def execute_cb(self, goal):
-        # helper variables
-        r = rospy.Rate(1)
-        success = True
         
         # append the seeds for the fibonacci sequence
         self._feedback.sequence = []
         self._feedback.sequence.append(0)
         self._feedback.sequence.append(1)
         
-        # publish info to the console for the user
         rospy.loginfo('%s: Executing, creating WaitForUserInput sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
         
         # start executing the action
-        for i in range(1, goal.order):
-            # check that preempt has not been requested by the client
-            if self._as.is_preempt_requested():
-                rospy.loginfo('%s: Preempted' % self._action_name)
-                self._as.set_preempted()
-                success = False
-                break
-            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-            # publish the feedback
-            self._as.publish_feedback(self._feedback)
-            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-            r.sleep()
+        success = fibonacci_example(self, success)
+          
+        if success:
+            self._result.sequence = self._feedback.sequence
+            rospy.loginfo('%s: Succeeded' % self._action_name)
+            self._as.set_succeeded(self._result)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+# GetData
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+class GetData(object):
+    
+    _feedback = robogenqbo.msg.GetDataFeedback() #create feedback message
+    _result = robogenqbo.msg.GetDataResult() #create result message
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.GetDataAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
+      
+    def execute_cb(self, goal):
+        
+        # append the seeds for the fibonacci sequence
+        self._feedback.sequence = []
+        self._feedback.sequence.append(0)
+        self._feedback.sequence.append(1)
+        
+        rospy.loginfo('%s: Executing, creating GetData sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
+        
+        # start executing the action
+        success = fibonacci_example(self, success)
+          
+        if success:
+            self._result.sequence = self._feedback.sequence
+            rospy.loginfo('%s: Succeeded' % self._action_name)
+            self._as.set_succeeded(self._result)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+# SetData
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+class SetData(object):
+    
+    _feedback = robogenqbo.msg.SetDataFeedback() #create feedback message
+    _result = robogenqbo.msg.SetDataResult() #create result message
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, robogenqbo.msg.SetDataAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
+      
+    def execute_cb(self, goal):
+        
+        # append the seeds for the fibonacci sequence
+        self._feedback.sequence = []
+        self._feedback.sequence.append(0)
+        self._feedback.sequence.append(1)
+        
+        rospy.loginfo('%s: Executing, creating SetData sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
+        
+        # start executing the action
+        success = fibonacci_example(self, success)
           
         if success:
             self._result.sequence = self._feedback.sequence
@@ -99,7 +151,10 @@ class WaitForUserInput(object):
 #------------------------------------------------------------------------------------------------------------------------------------------------------------	 
 if __name__ == '__main__':
     rospy.init_node('qbo')
-    server = VoiceOutput(rospy.get_name()) # example call
+    server1 = VoiceOutput(rospy.get_name())
+    #server2 = WaitForUserInput(rospy.get_name())
+    #server3 = GetData(rospy.get_name())
+    #server4 = SetData(rospy.get_name())
     rospy.spin()
 	
 	
