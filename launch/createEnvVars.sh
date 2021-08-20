aa3cdd536e2ef390010318916e5645e8c3d4dcd9
SRFG-MAT <mathias.schmoigl@salzburgresearch.at>
2021-08-20 14:45:09 +0200

    added ROS_IP variable to environment script

--- launch/createEnvVars.sh
+++ launch/createEnvVars.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export ROS_IP="192.168.48.117"
 export ROS_MASTER_URI="http://192.168.48.41:11311/"
 export ROS_PYTHON_LOG_CONFIG_FILE="/opt/QBO/catkin_ws/src/RoboGen-QBO/logging/logging.conf"
 echo "[createEnvVars.sh] Succesfully exported ROS env variables for this terminal"
